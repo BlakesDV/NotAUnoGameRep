7732c9b7d9ab9927b2e7953ed6970fd29babeffa
BlakesDV <dmunoz@cocoschool.mx>
2024-05-15 14:27:14 -0600

    MultiColor Draw
    Basic multicolored draw for cards (not ready)
    todo: multiple numbers to be added

--- NotUnoTheGame/Assets/Scripts/CardDrawer.cs
+++ NotUnoTheGame/Assets/Scripts/CardDrawer.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CardDrawer : MonoBehaviour
 {
-    public GameObject cardFront;
-    public GameObject cardBack;
+    public GameObject blueCards;
+    public GameObject redCards;
+    public GameObject yellowCards;
+    public GameObject greenCards;
     public GameObject player1Panel;
     public GameObject player2Panel;
+    List<GameObject> cards = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +21,25 @@ public class CardDrawer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cards.Add(blueCards);
+        cards.Add(redCards);
+        cards.Add(yellowCards);
+        cards.Add(greenCards);
     }
 
     public void StartingDraw()
     {
-        for (var i = 0; i <  5; i++) {
-            GameObject player1Card = Instantiate(cardFront);
+        for (var i = 0; i <  7; i++) {
+            GameObject player1Card = Instantiate(cards[Random.Range(0, cards.Count)]);
             player1Card.transform.SetParent(player1Panel.transform, false); 
-            GameObject player2Card = Instantiate(cardFront);
+            GameObject player2Card = Instantiate(cards[Random.Range(0, cards.Count)]);
             player2Card.transform.SetParent(player2Panel.transform, false);
         }
     }
 
     public void Player1DrawOne()
     {
-        GameObject player1Card = Instantiate(cardFront);
+        GameObject player1Card = Instantiate(cards[Random.Range(0, cards.Count)]);
         player1Card.transform.SetParent(player1Panel.transform, false);
 
         // Opcional: Ajustar la posición local dentro del panel si es necesario
@@ -41,7 +47,7 @@ public class CardDrawer : MonoBehaviour
     }
     public void Player2DrawOne()
     {
-        GameObject player2Card = Instantiate(cardFront);
+        GameObject player2Card = Instantiate(cards[Random.Range(0, cards.Count)]);
         player2Card.transform.SetParent(player2Panel.transform, false);
 
         // Opcional: Ajustar la posición local dentro del panel si es necesario
